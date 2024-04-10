13730b51be3841babdd77c263417516eaa83e1c5
Christian Krinitsin <code@krinitsin.xyz>
2024-04-10 16:08:41 +0200

    rust nvim

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -170,4 +170,12 @@ return {
        end,
     },
 
+    {
+        "folke/trouble.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+          vim.keymap.set("n", "<C-c>", ":TroubleToggle<CR>")
+        end,
+    },
+
 }
--- nvim/.config/nvim/lua/plugins/rust.lua
+++ nvim/.config/nvim/lua/plugins/rust.lua
@@ -1,30 +1,13 @@
 return {
+
     {
         "simrat39/rust-tools.nvim",
         config = function ()
-            require("rust-tools").setup({
-                tools = {
-                    hover_actions = {
-                        winhighlight = "NormalCmpDoc",
-                        border = {
-                            { "╭", "CmpDocBorder" },
-                            { "─", "CmpDocBorder" },
-                            { "╮", "CmpDocBorder" },
-                            { "│", "CmpDocBorder" },
-                            { "╯", "CmpDocBorder" },
-                            { "─", "CmpDocBorder" },
-                            { "╰", "CmpDocBorder" },
-                            { "│", "CmpDocBorder" },
-                        },
-                        auto_focus = true,
-                    },
-                },
-                server = {
-                    on_attach = function(_, bufnr)
-                        vim.keymap.set("n", "<C-space>", require("rust-tools").hover_actions.hover_actions, { buffer = bufnr })
-                    end,
-                },
-            })
+            require("rust-tools").setup()
+            vim.keymap.set("n", '<leader>rr', ':lua require("rust-tools").runnables.runnables()<CR>', {})
+            vim.keymap.set("n", '<leader>rt', ':lua require("rust-tools").open_cargo_toml.open_cargo_toml()<CR>', {})
+            vim.keymap.set("n", '<leader>rd', ':!cargo doc --open <CR>', {})
         end
     }
+
 }
