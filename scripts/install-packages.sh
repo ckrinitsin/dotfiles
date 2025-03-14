c8ff2e669772d57cbfdedfdbd2f6237a3c8cf96a
Christian Krinitsin <code@krinitsin.xyz>
2025-03-14 20:57:57 +0100

    add packages, edit signing key and edit mail api

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # list of all needed pacman packages
-declare -a pacman=("platformio-core" "platformio-core-udev" "whois" "bash-completion" "fzf" "grim" "pacman-contrib" "sway-contrib" "python-tldextract" "pass" "gnupg" "base-devel" "libnotify" "wl-clipboard" "qt6-wayland" "xorg-xwayland" "nerd-fonts" "zoxide" "waybar" "bison" "startup-notification" "flex" "wayland-protocols" "pkgconf" "cmake" "gcc" "alacritty" "dunst" "neovim" "qutebrowser" "starship" "xdg-user-dirs" "zathura" "zathura-pdf-mupdf" "meson" "ninja" "inotify-tools" "pipewire" "pipewire-audio" "pipewire-alsa" "pipewire-pulse" "pavucontrol" "texlive-basic" "texlive-bibtexextra" "texlive-latex" "texlive-mathscience" "texlive-latexrecommended" "texlive-latexextra" "texlive-binextra")
+declare -a pacman=("swaybg" "syncthing" "openssh" "lazygit" "platformio-core" "platformio-core-udev" "whois" "bash-completion" "fzf" "grim" "pacman-contrib" "sway-contrib" "python-tldextract" "pass" "gnupg" "base-devel" "libnotify" "wl-clipboard" "qt6-wayland" "xorg-xwayland" "nerd-fonts" "zoxide" "waybar" "bison" "startup-notification" "flex" "wayland-protocols" "pkgconf" "cmake" "gcc" "alacritty" "dunst" "neovim" "qutebrowser" "starship" "xdg-user-dirs" "zathura" "zathura-pdf-mupdf" "meson" "ninja" "inotify-tools" "pipewire" "pipewire-audio" "pipewire-alsa" "pipewire-pulse" "pavucontrol" "texlive-basic" "texlive-bibtexextra" "texlive-latex" "texlive-mathscience" "texlive-latexrecommended" "texlive-latexextra" "texlive-binextra")
 
 # list of all needed aur packages
 declare -a aur=()
@@ -11,7 +11,7 @@ do
     pacman -Q | grep "$package" 1>/dev/null
     if [ $? != 0 ]; then
         printf "Installing '$package'\n"
-        sudo pacman -S "$package"
+        sudo pacman -S --noconfirm "$package"
     else
         printf "'$package' is already installed\n"
     fi
@@ -27,7 +27,6 @@ do
         printf "'$package' is already installed\n"
     fi
 done
-exit
 
 # install rofi and dmenu for wayland
 git clone https://github.com/lbonn/rofi.git /tmp/rofi
